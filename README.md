32aa8310ca600404b35de322f9e15ab14e5b807f
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2019-12-30 21:01:38 +0000

    Add Active DIrectory integration. Use DnsServer module when present.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Windows-DNS-AdBlocker
 
-A simple to use PowerShell script that uses an adblocking list and imports it into the Windows Server DNS entry list to block them. To be used as a scheduled task to run and update entries. I suggest running it every weekend.
+A simple to use PowerShell script that uses an open source adblocking list and imports it into the Windows Server DNS entry list to block them. To be used as a scheduled task to run and update entries. I suggest running it every weekend.
+
+For Windows 2012 Server and above, this script uses the `DnsServer` PowerShell module to make changes to the DNS, and if Active Directory is detected then it can create AD-integrated DNS zones that will replicate forest-wide. For earlier versions of Windows, only the local DNS will be updated via direct modification of the registry - if you have more than one DNS server you will need to run this script at all of them.
 
 We installed this script as a security measure and to ease deployment for all users that do not have the required rights to install an adblocker or the knowledge to do so when they want to. This way the DNS server controls everything and no need for per client installation or management.
 
 ## Caution
 
-There is code included to clean up the registry of old DNS entries to keep the AdBlock list up to date. This should be OK as it explicitly checks for
-existence of the property `DatabaseFile` having a value of `adservers.dns` on each zone entry but be sure to test it first, having a backup of your valid zones!
+There is code included to clean up the registry of old DNS entries to keep the AdBlock list up to date. This should be OK as it explicitly checks for existence of the property `DatabaseFile` having a value of `adservers.dns` on each zone entry but be sure to test it first, having a backup of your valid zones!
+
+Where Active Directory integration is used, the `Responsible Person` field of the SOA record is set to a GUID value that is checked for on deletion, so should in theory be very safe.
 
 ## Tested
 
@@ -15,18 +18,20 @@ The script has been tested in our own environment (company-wide) on a Windows Se
 
 ## AdBlock List
 
-The script currently uses a very specific AdBlock list from https://pgl.yoyo.org/adservers/ they also have a great explanation about how to setup a Windows DNS AdBlocker on the website on this page https://pgl.yoyo.org/adservers/#other and navigating to the "Microsoft DNS Server" section. You will want to read it because you will require some pre-setup before the script can run fully automated.
+The script currently uses a very specific AdBlock list from https://pgl.yoyo.org/adservers/ they also have a great explanation about how to setup a Windows DNS AdBlocker on the website on this page https://pgl.yoyo.org/adservers/#other and navigating to the "Microsoft DNS Server" section. You will want to read it because you may require some pre-setup before the script can run fully automated.
 
 Currently in the script we are using this specific AdBlock file:
 https://pgl.yoyo.org/adservers/serverlist.php?hostformat=win32reg-sp4&showintro=0&mimetype=plaintext
 
 ## Adservers.dns file
 
-The file included in this repo called adservers.dns is a file that should be copied to %SystemRoot%\system32\dns as a reference of where the entry detected needs to go when a ad has been detected. The one that I added routes everything to localhost making them not appear but you could customize this. The script automatically downloads the default adservers.dns file when has detected it is not found.
+The file included in this repo called `adservers.dns` is a file that should be copied to `%SystemRoot%\system32\dns` as a reference of where the entry detected needs to go when a ad has been detected. The one that I added routes everything to localhost making them not appear but you could customize this. The script automatically downloads the default `adservers.dns` file when has detected it is not found.
+
+This file is used only for non-Active Directory integrated deployments. For AD-integrated mode, the default A record entries are pointed to the same location (localhost) as that in `adservers.dns`. Currently, if you wish to change this you will need to edit the code.
 
 ## Cleaning up
 
-If you want to remove all the adblock zones from your DNS, run this script again with the `-Remove` switch. Please pay attention to the `Caution` paragraph above.
+If you want to remove all the adblock zones from your DNS, run this script again with the `-Remove` switch. Please pay attention to the `Caution` paragraph above, especially if you have created the zones in non-Active Directory integrated mode.
 
 ## License
 
--- Windows-DNS-AdBlocker.ps1
+++ Windows-DNS-AdBlocker.ps1
@@ -2,13 +2,23 @@
     .SYNOPSIS
         Populate Windows DNS with black-hole entries for known ad-server domains
 
+    .DESCRIPTION
+        Use DNS to block known ad-server domains by redirecting requests to a black hole (localhost)
+        This needs to be run with administrator privilege, and if using Active Directory integration,
+        you should run this under an account with domain admin privilege.
+
     .PARAMETER Remove
         If set, remove all ad-server entries from the DNS
         If not set, update DNS with latest ad-server entries.
+
+    .PARAMETER ActiveDirectoryIntegrated
+        If set, detect Active Directory and create AD integrated zones if found.
+        Integrated zones will replicate to all DNS servers in AD forest.
 #>
 param
 (
-    [switch]$Remove
+    [switch]$Remove,
+    [switch]$ActiveDirectoryIntegrated
 )
 
 Write-Host "===================================================="
@@ -32,23 +42,90 @@ function Insert-Content ($file)
 }
 
 #Declares
+$artifactPath = Join-Path $env:TEMP "DNS Blacklist"
 $url = "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=win32reg-sp4&showintro=0&mimetype=plaintext"
 $adServerZoneFile = "adservers.dns"
 $adserversurl = "https://raw.githubusercontent.com/perplexityjeff/Windows-DNS-AdBlocker/master/$adServerZoneFile"
-$adserverstemp = "C:\DNS Blacklist\$adServerZoneFile"
-$adserverslocation = "C:\Windows\System32\dns\$adServerZoneFile"
+$adserverstemp = Join-Path $artifactPath $adServerZoneFile
+$adServersZoneFileLocation = Join-Path $env:SYSTEMROOT (Join-Path "System32\dns" $adServerZoneFile)
 
 $date = Get-Date -format "yyyyMMdd"
-$blacklist = "C:\DNS Blacklist\Blacklist_" + $date + ".reg"
-$path = "C:\DNS Blacklist\"
+$blacklist = Join-Path $artifactPath ("Blacklist_" + $date + ".reg")
 $limit = (Get-Date).AddDays(-15)
+
+# How we will identify Active Directory integrated zones created by this tool.
+$responsiblePerson = "adserver-71e5831a-aba8-4890-9037-399cb92de586"
+
+# Detect Active Directory and DnsServer PowerShell module
+$useActiveDirectory = $false
+$activeDirectoryDetected = $null -ne $env:LOGONSERVER -and $null -ne $env:USERDOMAIN
+$haveDnsServerModule = $null -ne (Get-Module -ListAvailable DnsServer)
 
 try
 {
+    # Test for admin rights
+    if (-not (New-Object Security.Principal.WindowsPrincipal -ArgumentList ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
+    {
+        throw "Must be Administrator to run this program."
+    }
+
     # Test if DNS server exists on this host
     if (-not (Get-Service -Name DNS -ErrorAction SilentlyContinue))
     {
-        throw "Local DNS server not found"
+        throw "Local DNS server not found. Please run on a server that hosts DNS."
+    }
+
+    if (-not ([Environment]::UserInteractive))
+    {
+        # Hide progress bars if running e.g. in task scheduler.
+        $ProgressPreference = 'SilentlyContinue'
+    }
+
+    if ($haveDnsServerModule)
+    {
+        # For Windows 2012 and above, use DnsServer cmdlets
+        Write-Host "Importing DnsServer module"
+        Import-Module DnsServer
+    }
+
+    if ($activeDirectoryDetected -and $haveDnsServerModule -and ([Environment]::UserInteractive) -and -not ($ActiveDirectoryIntegrated -or $Remove))
+    {
+        # Recommend AD integration if not selected.
+        $choice = $host.ui.PromptForChoice(
+            "Active Directory detected. It is recommended to use it as it will replicate zones to all DNS servers in the forest. Use Active Directory?",
+            $null,
+            @(
+                New-Object System.Management.Automation.Host.ChoiceDescription ('&Yes', "Use Active Directory." )
+                New-Object System.Management.Automation.Host.ChoiceDescription ('&No', 'Use file-based zones (no replication).')
+            ),
+            0
+        )
+
+        if ($choice -eq 0)
+        {
+            $useActiveDirectory = $true
+        }
+    }
+
+    if ($ActiveDirectoryIntegrated)
+    {
+        # If user asked for Active Directory integration, check we can actually do it.
+        if (-not $activeDirectoryDetected)
+        {
+            Write-Warning "Active Directory domain not detected, or you are not using a domain login. Falling back to file-based zones"
+        }
+        else
+        {
+            # Check we have the required PowerShell module for integration
+            if ($haveDnsServerModule)
+            {
+                $useActiveDirectory = $true
+            }
+            else
+            {
+                Write-Warning "PowerShell module DnsServer not detected. Cannot use AD integration. Perhaps this version of Windows is too old. Falling back to file-based zones"
+            }
+        }
     }
 
     if ($Remove)
@@ -57,21 +134,21 @@ try
     }
     else
     {
-        #Testing if DNS Blacklist folder exists
+        # Testing if DNS Blacklist folder exists
         Write-Host "Detecting if download location exists..."
-        if (Test-Path $path)
+        if (Test-Path $artifactPath)
         {
             Write-Host "Download location exists"
         }
         else
         {
-            New-Item $path -ItemType directory
+            New-Item $artifactPath -ItemType directory
             Write-Host "Download location has been created"
         }
 
-        #Testing if $adServerZoneFile file exists
+        # Testing if $adServerZoneFile file exists
         Write-Host "Detecting if $adServerZoneFile file exists..."
-        if (-Not(Test-Path $adserverslocation))
+        if (-not (Test-Path $adServersZoneFileLocation))
         {
             Write-Host "Downloading default $adServerZoneFile file..."
             $client = new-object System.Net.WebClient
@@ -91,7 +168,7 @@ try
             Write-Host "Downloaded default $adServerZoneFile file"
 
             Write-Host "Placing downloaded $adServerZoneFile file in the systemroot..."
-            Move-Item $adserverstemp $adserverslocation
+            Move-Item $adserverstemp $adServersZoneFileLocation
             Write-Host "Placed downloaded $adServerZoneFile file in the systemroot"
         }
         else
@@ -99,7 +176,7 @@ try
             Write-Host "Detected $adServerZoneFile file"
         }
 
-        #Testing if DNS Blacklist exists
+        # Testing if DNS Blacklist exists
         Write-Host "Detecting if older DNS Blacklist exists..."
         if (Test-Path $blacklist)
         {
@@ -112,11 +189,11 @@ try
             Write-Host "No existing DNS Blacklist found"
         }
 
-        #Downloading of the Adblock reg file
+        # Downloading of the Adblock reg file
         Write-Host "Downloading newest AdBlock file..."
-        if (-not (Split-Path -parent $path) -or -not (Test-Path -pathType Container (Split-Path -parent $path)))
+        if (-not (Split-Path -parent $artifactPath) -or -not (Test-Path -pathType Container (Split-Path -parent $artifactPath)))
         {
-            $path = Join-Path $pwd (Split-Path -leaf $path)
+            $artifactPath = Join-Path $pwd (Split-Path -leaf $artifactPath)
         }
         try
         {
@@ -134,7 +211,6 @@ try
                 }
             }
 
-            "REGEDIT4`n" | Insert-Content $blacklist
             Write-Host "Downloaded newest AdBlock file"
         }
         catch
@@ -149,44 +225,203 @@ try
         }
     }
 
-    #Stopping the DNS Server
-    Write-Host "Stopping DNS Server..."
-    Stop-Service -Name DNS
-    Write-Host "Stopped DNS Server"
+    if (-not $haveDnsServerModule)
+    {
+        # With DnsServer PowerShell module, we can operate on the DNS hot.
+        # If poking the registry, we need to do it cold.
+        # Stopping the DNS Server.
+        Write-Host "Stopping DNS Server..."
+        Stop-Service -Name DNS
+        Write-Host "Stopped DNS Server"
+    }
 
-    #Remove All Old Entries (CAUTION: Be sure to tweak this to your environment and not delete valid DNS entries)
-    Write-Host "Deleting old Blacklist entries from Registry"
-    Get-ChildItem "HKLM:\software\Microsoft\Windows NT\CurrentVersion\DNS Server\Zones\" |
-    ForEach-Object {
+    # Remove All Old Entries (CAUTION: Be sure to tweak this to your environment and not delete valid DNS entries)
+    Write-Host "Deleting old Blacklist entries"
+    $zoneKey = "HKLM:\software\Microsoft\Windows NT\CurrentVersion\DNS Server\Zones\"
 
-        $CurrentKey = Get-ItemProperty -Path $_.PsPath
+    # Read all zones _before_ modifying anything
+    if ($haveDnsServerModule)
+    {
+        # We can do some pre-filtering here
+        $allZones = Get-DnsServerZone | Where-Object { $_.ZoneType -eq 'Primary' -and -not $_.IsReverseLookupZone -and -not $_.IsAutoCreated }
+    }
+    else
+    {
+        $allZones = (Get-ChildItem -Path $zoneKey).Name |
+        Split-Path -Leaf |
+        ForEach-Object {
 
-        # Cleanly detect zones that are using adservers.dns
-        if ($CurrentKey.PSObject.Properties.Name -icontains 'DatabaseFile' -and $CurrentKey.DatabaseFile -ieq $adServerZoneFile)
-        {
-            Write-Verbose "Removing zone: ($CurrentKey.PSChildName)"
-            $CurrentKey | Remove-Item -Force #-Whatif
+            # Make these results look like a DNS Zone object returned by Get-DnsZerverZone
+            New-Object PSObject -Property @{
+                ZoneName = $_
+            }
         }
     }
 
-    Write-Host "Deleted old Blacklist entries from Registry"
+    # Further filter out zones matching the the user DNS domain and the TrustAnchors special domain
+    $allZones = $allZones |
+    Where-Object {
+
+        $_.ZoneName -ne 'TrustAnchors'
+    } |
+    Where-Object {
+
+        if ($null -eq $env:USERDNSDOMAIN)
+        {
+            # Include all if no DNS domain to compare with
+            $true
+        }
+        else
+        {
+            # Include those not matching the local DNS domain
+            -not $_.ZoneName.EndsWith($env:USERDNSDOMAIN, [StringComparison]::OrdinalIgnoreCase)
+        }
+    }
+
+    # Now do the deletions
+    # Use Measure-Object as it can count the input object being $null
+    $totalZones = ($allZones | Measure-Object).Count
+    $numRemoved = 0
+    $numProcessed = 0
+
+    $allZones |
+    ForEach-Object {
+
+        if ($numRemoved % 20 -eq 0)
+        {
+            # Update progress every 20 zones
+            $percentComplete = [int](($numProcessed / $totalZones) * 100)
+            Write-Progress -Activity "Deleting adserver DNS zones" -Status "$($percentComplete)% Complete:" -PercentComplete $percentComplete
+        }
+
+        if ($haveDnsServerModule)
+        {
+            $zone = Get-DnsServerZone -Name $_.ZoneName -ErrorAction SilentlyContinue
+
+            if ($null -ne $zone)
+            {
+                # Is it one of ours?
+                if ($zone.IsDsIntegrated)
+                {
+                    # AD Integrated - Identify by the value we set for Responsible Person in the SOA record...
+
+                    # Get SOA record
+                    $soa = $zone | Get-DnsServerResourceRecord -RRType SOA
+
+                    if ($soa.RecordData.ResponsiblePerson.StartsWith($responsiblePerson))
+                    {
+                        # Delete it.
+                        Write-Verbose "Removing zone: $($_.ZoneName)"
+                        $zone | Remove-DnsServerZone -Force
+                        ++$numRemoved
+                    }
+                }
+                else
+                {
+                    # File zone - Identify by the zone filename
+                    if ($zone.ZoneFile -eq $adServerZoneFile)
+                    {
+                        # Delete it.
+                        Write-Verbose "Removing zone: $($_.ZoneName)"
+                        $zone | Remove-DnsServerZone -Force
+                        ++$numRemoved
+                    }
+                }
+            }
+
+            ++$numProcessed
+        }
+        else
+        {
+            $CurrentKey = Get-ItemProperty -Path (Join-Path $zoneKey $_.ZoneName)
+
+            # Cleanly detect zones that are using adservers.dns
+            if ($CurrentKey.PSObject.Properties.Name -icontains 'DatabaseFile' -and $CurrentKey.DatabaseFile -ieq $adServerZoneFile)
+            {
+                Write-Verbose "Removing zone: $($CurrentKey.PSChildName)"
+                $CurrentKey | Remove-Item -Force #-Whatif
+                ++$numRemoved
+            }
+        }
+    }
+
+    # Import new adserver zones
+
+    # Clear progress bar
+    Write-Progress -Activity "Deleting adserver DNS zones" -Status "100% Complete:" -PercentComplete 100 -Completed
+    Write-Host "Deleted $numRemoved old Blacklist entries from Registry"
 
     if (-not $Remove)
     {
-        #Importing the file into regedit
         Write-Host "Importing AdBlock file..."
-        regedit.exe /s $blacklist
-        Write-Host "Imported AdBlock file"
+
+        if ($haveDnsServerModule)
+        {
+            $numAdded = 0
+
+            # Parse the REG file for domains and add them
+            $domains = Get-Content $blacklist |
+            Foreach-Object {
+
+                if ($_ -match '\\(?<domain>[^\\]+)\]\s*$')
+                {
+                    $Matches.domain
+                }
+            }
+
+            $totalZones = ($domains | Measure-Object).Count
+
+            $domains |
+            Foreach-Object {
+
+                if ($numAdded % 20 -eq 0)
+                {
+                    # Update progress every 20 zones
+                    $percentComplete = [int](($numAdded / $totalZones) * 100)
+                    Write-Progress -Activity "Adding adserver DNS zones" -Status "$($percentComplete)% Complete:" -PercentComplete $percentComplete
+                }
+
+                if ($useActiveDirectory)
+                {
+                    # Create Active Directory integrated zone and add blackhole records
+                    # Set the responsible person field in the SOA record to our magic value for easy indentification on delete.
+                    $zone = Add-DnsServerPrimaryZone -Name $_ -ResponsiblePerson $responsiblePerson -ReplicationScope Forest -PassThru
+                    $zone | Add-DnsServerResourceRecordA -IPv4Address 127.0.0.1 -Name '*' -TimeToLive 01:00:00
+                    $zone | Add-DnsServerResourceRecordA -IPv4Address 127.0.0.1 -Name '@' -TimeToLive 01:00:00
+                }
+                else
+                {
+                    # Create zone and point to our zone file
+                    Add-DnsServerPrimaryZone -Name $_ -ZoneFile $adServerZoneFile -LoadExisting -ResponsiblePerson $responsiblePerson
+                }
+
+                ++$numAdded
+            }
+
+            # Clear progress bar
+            Write-Progress -Activity "Adding adserver DNS zones" -Status "100% Complete:" -PercentComplete 100 -Completed
+            Write-Host "Imported $numAdded zones from AdBlock file"
+        }
+        else
+        {
+            # Importing the file into regedit
+            "REGEDIT4`n" | Insert-Content $blacklist
+            regedit.exe /s $blacklist
+            Write-Host "Imported AdBlock file"
+        }
     }
 
-    #Starting the DNS Server
-    Write-Host "Starting DNS Server..."
-    Start-Service -Name DNS
-    Write-Host "Started DNS Server"
+    if ((Get-Service -Name DNS).Status -ne 'Running')
+    {
+        # Starting the DNS Server
+        Write-Host "Starting DNS Server..."
+        Start-Service -Name DNS
+        Write-Host "Started DNS Server"
+    }
 
     #Removing Blacklist files older then 15 days
     Write-Host "Removing old AdBlock files..."
-    Get-ChildItem -Path $path -Recurse -Force | Where-Object { -not $_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+    Get-ChildItem -Path $artifactPath -Recurse -Force | Where-Object { -not $_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
     Write-Host "Removed old AdBlock files"
 
     #Script has been completed
